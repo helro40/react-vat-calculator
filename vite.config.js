069b3d7ddefa11ace76e22e03ca2c3ddc9ffab7d
helro40 <u6540159@au.edu>
2025-07-06 20:13:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default ({
   base: '/react-vat-calculator/',
   plugins: [react()],
 })
